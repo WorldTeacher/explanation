243182484bb29b668c2ba11c627184ed98ae1af9
WorldTeacher <kirchneralexander020@gmail.com>
2022-04-26 15:36:32 +0200

    .

--- Python Programme/for/README.txt
+++ Python Programme/for/README.txt
@@ -1,1 +1,9 @@
 In diesem Ordner sind Beispiele für for-loops
+
+for-loop.py ausführen und nach Aufforderung einen der Möglichen Inputs eingeben:
+
+integers # Zahlen
+
+strings # Strings
+
+arrays # Ein Beispiel, dass auch Arrays verarbeitet werden können
--- Python Programme/for/for-loop.py
+++ Python Programme/for/for-loop.py
@@ -1,5 +1,6 @@
 #einfacher for-loop
 __name__ = input('Name of the program: ')
+#define functions
 #int
 def integers():
     for i in range(5): #für jeden Wert von 0 bis 4, wird i durchgezählt und gedruckt
@@ -9,8 +10,18 @@ def integers():
 def strings():
     for i in 'Hello World': #für jedes Zeichen in der Zeichenkette, wird i durchgezählt und gedruckt
         print(i)
-
+#array
+def arrays():
+    arr=[{'name':'John', 'age':23}, {'name':'Jane', 'age':24}, {'name':'Jack', 'age':25}] #ein Array mit 3 Objekten
+    for i in arr: #für jeden Wert in der Liste, wird i durchgezählt und gedruckt
+        print(i)
+#start of program
 if __name__ == "integers":
     integers()
 elif __name__ == "strings":
     strings()
+elif __name__ == "arrays":
+    arrays()
+else:
+    print("Nothing")
+        
